e53f24d5901dbdb76c41e02e3fb78e85388de17e
Evka K <kuklaewa@gmail.com>
2021-04-19 11:00:31 +0000

    feat:add info to foodDrinks array and make it functional on the map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,4 @@
-/* CREDIT: Google map documentation used and customized link:https://developers.google.com/maps/documentation*/
+/* CREDIT: Google map documentation used and and Code institute lessons and customized link:https://developers.google.com/maps/documentation*/
     
 
 //Add the map of Poland
@@ -90,6 +90,7 @@ function initMapAttractions() {
         },
     ];
 // add click listener to each marker which opens the InfoWindow with attractionsMarkers info
+//<!--CREDIT: solution for Integrate Google Maps MarkerClusterer with infowindow from stackoverflow
         var infowindow = new google.maps.InfoWindow();
 
         var markers = attractionsMarkers.map(function (attractionsMarkers, i) {
@@ -100,7 +101,7 @@ function initMapAttractions() {
         google.maps.event.addListener(marker, 'click', function(evt) {
             infowindow.setContent(attractionsMarkers.info);
             infowindow.open(attractionsMap, marker);
-        })
+        });
         return marker;
 
     });
@@ -111,73 +112,80 @@ function initMapAttractions() {
     });
 }
 
- // restaurants button
-document.getElementById("btn-restaurants").addEventListener("click",initMapRestaurants);
-function initMapRestaurants() {
-    var RestaurantsMap = new google.maps.Map(document.getElementById("map"), {
+//Add the map of Wroclaw with locations of the FoodDrinks when pressed on the button 'Food&Drinks'
+document.getElementById("btn-food-drinks").addEventListener("click",initMapfoodDrinks);
+function initMapfoodDrinks() {
+    var foodDrinksMap = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: {lat:51.1079, lng:17.0385},
     });
 
-
 var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var restaurantsMarkers = [
+ // Add markers to foodDrinksMarkers Array
+let foodDrinksMarkers = [
     {
-         lat:51.1092, lng:17.0320, 
-    
+        location: {lat:51.1092, lng:17.0320}, 
+        info: "<h5>Whiskey in the Jar</h5><p> Unique place, high-class steakhouse, offering beefsteaks of highest value, prepared on lava grill</p>"
     },
     {
-        lat:51.1100, lng:17.0312, 
-      
+        location: {lat:51.1100, lng:17.0312}, 
+        info: "<h5>Spiż - Microbrewery and Restaurant</h5><p>A brewery opened in the underground is one of the favourite places to relax. Despite the beer that is brewed there, according to the original recipie, in the basements of the Town Hall there is a restaurant.</p>"
     },
     {
-         lat:51.1094, lng:17.0304, 
-   
+        location: {lat:51.1094, lng:17.0304}, 
+        info: "<h5>Solny 20 Restaurant</h5><p> The cosy restaurant (expanded with an outdoor patio) is an excellent place for fast breakfast, nourishing lunch, an afternoon meeting with friends and a romantic evening for two.</p>"
     },
     {
-         lat:51.1095, lng:17.0305, 
- 
+        location: {lat:51.1095, lng:17.0305}, 
+        info: "<h5>Novocaina Restaurant</h5><p>One of the most recognised and trusted Wrocław restaurants. The venue offers a rich variety of traditional Mediterranean dishes and a selection of regional dished from northern Italy.</p>"
     },
     {
-         lat:51.1083, lng:17.0362, 
-      
+        location: {lat:51.1083, lng:17.0362}, 
+        info:"<h5>The restaurant “Hubertus”</h5><p>Traditional Polish cuisine, with natural ingredients and a wide range of game dishes. It has existed for 40 years now. For example, it serves a portion of boar or venison, as you like and according to the season.</p>"
     },
     {
-        lat:51.1083, lng:17.0262, 
-
+        location: {lat:51.1083, lng:17.0262}, 
+        info:"<h5>Ślepy Zaułek</h5><p>Bar & restaurant in secluded Pasaż Pokoyhof. In the menu there are lunches, burgers and grilled dishes, Italian, Mediterranean and vegetarian cuisine</p>"
     },
     {
-        lat:51.1083, lng:17.0354, 
-       
+        location: {lat:51.1083, lng:17.0354}, 
+        info:"<h5>Vertigo Jazz Club & Restaurant</h5><p>Vertigo club and restaurant. Almost everyday the club hosts jazz concerts.</p>"
     },
     {
-        lat:51.1105, lng:17.0260, 
-      
+        location: {lat:51.1105, lng:17.0260}, 
+        info:"<h5>Machina Organika</h5><p>The restaurant offers healthy food, in menu there arebreakfasts, lunches and desserts. The restaurant is located near Wrocław’s Rynek.</p>"
     },
     {
-        lat:51.1114, lng:17.0292, 
-    
+        location: {lat:51.1114, lng:17.0292}, 
+        info:"<h5>Ambasada Bar</h5><p>Ambasada is a place in the heart of the city centre, where the trails of people from Wrocław and tourists exploring party side of Wrocław cross each other. The place serves simple dishes and affordable drinks</p>"
     },
     {
-        lat:51.1128, lng:17.0323, 
+        location: {lat:51.1128, lng:17.0323}, 
+        info:"<h5>Kociołek bar&grill</h5><p> The speciality of Kociołek bar&grill are the meals from oven, meat, fish, vegetable dishes.  </p>"
     }
 ];
+// add click listener to each marker which opens the InfoWindow with foodDrinksMarkers info
+//<!--CREDIT: solution for Integrate Google Maps MarkerClusterer with infowindow from stackoverflow
+var infowidow = new google.maps.InfoWindow();
 
-
-var markers = restaurantsMarkers.map(function (location, i) {
-    return new google.maps.Marker({
-        position:location,
+var markers = foodDrinksMarkers.map(function (foodDrinksMarkers, i) {
+var marker =  new google.maps.Marker({
+        position:foodDrinksMarkers.location,
         label: labels[i % labels.length]
     });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowidow.setContent(foodDrinksMarkers.info);
+        infowidow.open(foodDrinksMap, marker);
+    })
+    return marker;
 });
 
-new MarkerClusterer(RestaurantsMap, markers, {
+var marker = new MarkerClusterer(foodDrinksMap, markers, {
     imagePath:
             "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
 });
 }
-
+/*
 // accomodation button
 document.getElementById("btn-accomodation").addEventListener("click",initMapAccomodation);
 function initMapAccomodation() {
@@ -313,61 +321,6 @@ new MarkerClusterer(dwarfsMap, markers, {
 
 
 
-/*
-var attractionsMarkers = [
-    {
-        location: {lat:51.1008, lng:17.0300}, 
-        info:"<h5>Capitol</h5><p>The Capitol Musical Theater offers entertainment and bold reflection</p>"
-    },
-    {
-        location: {lat:51.1044, lng:17.0752}, 
-        info:"<h5>Wrocław Africarium</h5><p>Spacious zoo with exhibits of 1,000 animals and a large oceanarium with around 5,000 animals.</p>",
-    },
-    {
-        location: {lat:51.1101, lng:17.0443}, 
-        info:"<h5>Panorama of the Battle of Raclawice</h5><p>The Monumental cycloramic painting depicting the Battle of Racławice, during the Kościuszko Uprising.</p>",
-    },
-    {
-        location: {lat:51.1142, lng:17.0466}, 
-        info:"<h5>Cathedral of St. John the Baptist</h5><p> The cathedral, located in the Ostrów Tumski district, is a Gothic church with Neo-Gothic additions </p>"
-    },
-    {
-        location: {lat:51.1088, lng:17.0789}, 
-        info:"<h5> Wrocław Multimedia Fountain</h5><p>The multimedia musical fountain and ornamental pond, runs only during the summer season </p>"
-    },
-    {
-        location: {lat:51.0945, lng:17.0196}, 
-        info:"<h5>Kolejkowo</h5><p> Artistic venue featuring a range of detailed miniature worlds with model trains & local landmarks.</p>"
-    },
-    {
-        location: {lat:51.1055, lng:17.0320}, 
-        info:"<h5>The Wrocław Opera</h5><p>The opera house was opened in 1841 and up to 1945 was named after the city's then German name, Oper Breslau</p>"
-    },
-    {
-        location: {lat:51.1137, lng:17.0334}, 
-        info:"<h5>Museum of the University of Wroclaw</h5><p>The museum in  displaying many of the amazing historical objects: Aula Leopoldina, Oratorium Marianum,  Mathematical Tower</p>"
-    },
-    {
-        location: {lat:51.1098, lng:17.0327}, 
-        info:"<h5>Market Square</h5><p> The square is rectangular with the dimensions. It is one of the largest market squares in Europe, with the largest two city halls in the country.</p>"
-    },
-    {
-        location: {lat: 51.0897, lng:17.0172}, 
-        info:"<h5>Sky Tower Viewpoint</h5><p> Sky Tower is a skyscraper with the view point of Wroclaw on the top 49th floor in 1 minute with the lift.</p>"
-    },
-    {
-        location: {lat:51.1042, lng:17.0566}, 
-        info:"<h5>Hydropolis</h5><p>The Hydropolis Centre for Ecological Education, is a science centre dedicated to water. Its exhibitions use a variety of technology about the role of water in the environment.</p>"
-    },
-    {
-        location: {lat:51.1145, lng:17.0467}, 
-        info:"<h5>Ostrów Tumski</h5><p>The oldest, historic part of Wrocław, full of monuments, museums and beautiful flora that you can admire in the Botanical Garden.</p>"
-    },
-    {
-        location: {lat:51.1079, lng:17.0289}, 
-        info:"<h5>The City Museum of Wroclaw</h5><p> The Royal Palace is a palace in Wrocław, Poland. Originally a palace of the Prussian monarchy, it now houses the city museum.  </p>"
-    },
-]
 var acomodationMarkers = [
     {
         location: {lat:51.1092, lng:17.0377}, 
@@ -412,48 +365,7 @@ var acomodationMarkers = [
 ]
 
 
-var  = foodDrinksMarkers [
-    {
-        location: {lat:51.1092, lng:17.0320}, 
-        info: "<h5>Whiskey in the Jar</h5><p> Unique place, high-class steakhouse, offering beefsteaks of highest value, prepared on lava grill</p>"
-    },
-    {
-        location: {lat:51.1100, lng:17.0312}, 
-        info: "<h5>Spiż - Microbrewery and Restaurant</h5><p>A brewery opened in the underground is one of the favourite places to relax. Despite the beer that is brewed there, according to the original recipie, in the basements of the Town Hall there is a restaurant.</p>"
-    },
-    {
-        location: {lat:51.1094, lng:17.0304}, 
-        info: "<h5>Solny 20 Restaurant</h5><p> The cosy restaurant (expanded with an outdoor patio) is an excellent place for fast breakfast, nourishing lunch, an afternoon meeting with friends and a romantic evening for two.</p>"
-    },
-    {
-        location: {lat:51.1095, lng:17.0305}, 
-        info: "<h5>Novocaina Restaurant</h5><p>One of the most recognised and trusted Wrocław restaurants. The venue offers a rich variety of traditional Mediterranean dishes and a selection of regional dished from northern Italy.</p>"
-    },
-    {
-        location: {lat:51.1083, lng:17.0362}, 
-        info:"<h5>The restaurant “Hubertus”</h5><p>Traditional Polish cuisine, with natural ingredients and a wide range of game dishes. It has existed for 40 years now. For example, it serves a portion of boar or venison, as you like and according to the season.</p>"
-    },
-    {
-        location: {lat:51.1083, lng:17.0262}, 
-        info:"<h5>Ślepy Zaułek</h5><p>Bar & restaurant in secluded Pasaż Pokoyhof. In the menu there are lunches, burgers and grilled dishes, Italian, Mediterranean and vegetarian cuisine</p>"
-    },
-    {
-        location: {lat:51.1083, lng:17.0354}, 
-        info:"<h5>Vertigo Jazz Club & Restaurant</h5><p>Vertigo club and restaurant. Almost everyday the club hosts jazz concerts.</p>"
-    },
-    {
-        location: {lat:51.1105, lng:17.0260}, 
-        info:"<h5>Machina Organika</h5><p>The restaurant offers healthy food, in menu there arebreakfasts, lunches and desserts. The restaurant is located near Wrocław’s Rynek.</p>"
-    },
-    {
-        location: {lat:51.1114, lng:17.0292}, 
-        info:"<h5>Ambasada Bar</h5><p>Ambasada is a place in the heart of the city centre, where the trails of people from Wrocław and tourists exploring party side of Wrocław cross each other. The place serves simple dishes and affordable drinks</p>"
-    },
-    {
-        location: {lat:51.1128, lng:17.0323}, 
-        info:"<h5>Kociołek bar&grill</h5><p> The speciality of Kociołek bar&grill are the meals from oven, meat, fish, vegetable dishes.  </p>"
-    },
-]
+
 
 var dwarfsMarkers = [
     {
