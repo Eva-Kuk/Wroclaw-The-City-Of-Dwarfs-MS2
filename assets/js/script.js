c712c166ba7b234f149454120e28b2b5372ba3fc
Evka K <kuklaewa@gmail.com>
2021-04-25 11:42:17 +0000

    feat:add weather API to homoe page

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,40 @@ window.onscroll = function() {
     }
 
     this.prevScrollpos = currentScrollpos;
-}
+};
+
+
+// CREDIT from youtube tutorial how to build a weather API from Jonah Lawrence 
+let weather = {
+  apiKey: "f9f3ef1574ecab985a0fe573c8a1467f",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp} = data.main;
+    document.querySelector(".city").innerText = "Today in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+
+};
+weather.fetchWeather("Wroclaw");
+
