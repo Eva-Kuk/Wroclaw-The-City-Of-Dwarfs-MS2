2cecc52e0100fb3c16cb9b40ea62341c352d8e21
Evka K <kuklaewa@gmail.com>
2021-05-02 18:52:18 +0000

    add checking the code and beautifying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 /* CREDIT from w3school.com and customised for slide down a footer on scroll*/
 // Footer slide down on scroll
-let prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     let currentScrollpos = window.pageYOffset;
     if (prevScrollpos > currentScrollpos) {
